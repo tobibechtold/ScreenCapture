2ec14d5925d6a95a59b48e314121c04503270765
Tobias Bechtold <tobi.bechtold@gmail.com>
2014-01-30 23:30:58 +0100

    added ini File to set up path

--- src/com/asdfsoft/control/Main.java
+++ src/com/asdfsoft/control/Main.java
@@ -1,22 +1,46 @@
 package com.asdfsoft.control;
 
-import javax.swing.JFileChooser;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		JFileChooser j = new JFileChooser();
-		j.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		Integer opt = j.showSaveDialog(null);
-		if(opt == JFileChooser.APPROVE_OPTION) {
-			String path = j.getSelectedFile().getAbsolutePath();
-			CaptureThread captureThread = new CaptureThread(path);
-			new Thread(captureThread).start();
+		File f = new File("sc.ini");
+		
+		if(f.exists()) {
+			FileReader fr;
+			BufferedReader br = null; 
+			try {
+				fr = new FileReader(f);
+				br = new BufferedReader(fr);
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			String path = null;
+				try {
+					while(br.ready()) {
+						path = br.readLine();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				CaptureThread captureThread = new CaptureThread(path);
+				new Thread(captureThread).start();
+			}
+			
+
+			else {
+				CaptureThread captureThread = new CaptureThread("C:\\users\\");
+				new Thread(captureThread).start();
+			}
 		}
-		else {
-			CaptureThread captureThread = new CaptureThread("C://users//");
-			new Thread(captureThread).start();
-		}
-	}
+
+		
+	
 }
